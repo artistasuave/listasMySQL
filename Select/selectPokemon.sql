63d2a8e9ffe9df45372fb178e79b0cb86a3f5e45
Michelle Rogério <themicaloo@gmail.com>
2018-07-03 10:52:29 -0300

    SELECT

---
+++ Select/selectPokemon.sql
@@ -0,0 +1,14 @@
+USE lista02;
+
+#SELECT * FROM pokemons;
+
+#SELECT ataque, especial_ataque, defesa, especial_defesa FROM pokemons;
+
+#SELECT nome, categoria, ataque FROM pokemons ORDER BY ataque ASC;
+
+SELECT altura, peso, (peso/(altura*2) 'IMC' FROM pokemons;
+
+
+
+
+
