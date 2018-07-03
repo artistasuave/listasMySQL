1f3ba91d12e5f9107c82ae988dab1e90cf2b5000
Michelle Rogério <themicaloo@gmail.com>
2018-07-03 08:46:18 -0300

    enderecos

--- exemploPessoas.sql
+++ exemploPessoas.sql
@@ -12,7 +12,7 @@ CREATE TABLE pessoas(
 INSERT INTO pessoas VALUE ('Erick Bryan Enrico Vieira', '699.734.958-70' '90.745.276-0', '1989-05-28', '29');
 INSERT INTO pessoas VALUE ('Samuel Ruan Dias', '802.790.194-40', '55.318.691-7', '1988-02-08', '30');
 INSERT INTO pessoas VALUE('Fábio Benjamin Souza', '522.445.349-60', '54.715.232-2', '1970-03-22', '48');
-INSERT INTO pessoas VALUE('Elias Miguel Aparicio', '293.928.821-65', '12.680.444-8', '1986-06-07', '32'):
+INSERT INTO pessoas VALUE('Elias Miguel Aparicio', '293.928.821-65', '12.680.444-8', '1986-06-07', '32');
 INSERT INTO pessoas VALUE('Alexandre Gustavo Cardoso', '617.408.878-24', '8.888.574-4', '2003-04-15', '15');
 INSERT INTO pessoas VALUE('Kauê Lucca Duarte', '943.163.990-47', '46.977.509-9', '1965-06-15', '53');
 INSERT INTO pessoas VALUE('Marcos Bryan Guilherme Mendes', '955.129.235-95', '84.050.077-4', '2015-02-06', '3');
